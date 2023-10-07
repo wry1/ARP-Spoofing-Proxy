28fd86e94f2fedc95ad7a6ec73b0b19ae96445c9
James <wangruiyang4@gmail.com>
2023-10-07 15:40:52 +0800

    Update ARP Spoofing Proxy.sh
    Signed-off-by: James <wangruiyang4@gmail.com>

--- ARP Spoofing Proxy.sh
+++ ARP Spoofing Proxy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 #ARP Spoofing Proxy
-#Contributors: Wry, ChatGPT
+#Contributors: James, ChatGPT
 
 script_dir="$(dirname "$(readlink -f "$0")")"
-
 
 cd "$script_dir"
 bash "./.Modules/.Mac Changer.sh"
 bash "./.Modules/.ARP Adder.sh"
+sysctl net.ipv4.ip_forward=1
 python "./.Modules/.ARP Sender.py"
